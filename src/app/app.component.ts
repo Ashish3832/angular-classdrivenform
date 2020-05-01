fcb81a10cc34bb4422164add368f7aea8ee68fef
Ashish3832 <akumar6399@gmail.com>
2020-05-01 10:17:21 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,11 +7,42 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  registrationForm = new FormGroup(
+
+  constructor( private fb: FormBuilder){}
+  
+  registrationForm=this.fb.group({
+    userName: ['',[Validators.required,Validators.minLength(4)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postal: ['']
+    })
+  });
+  /*registrationForm = new FormGroup(
     {
       userName: new FormControl('Ashish'),
       password: new FormControl(''),
-      confirmPassword: new FormControl('')
+      confirmPassword: new FormControl(''),
+      address: new FormGroup({
+        city: new FormControl(''),
+        state: new FormControl(''),
+        postal: new FormControl('')
+      })
     }
   );
+  loadAPIdata()
+  {
+    this.registrationForm.setValue({
+      userName: 'rob',
+      password: 'rob@1234',
+      confirmPassword: 'rob@1234',
+      address: {
+        city: 'London',
+        state: 'England',
+        postal: '123456'
+      }
+    });
+  }*/
 }
